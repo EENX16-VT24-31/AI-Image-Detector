24097ab89c917fb936e31420f1464d07ecab5976
erwinia <joel.lagg@gmail.com>
2024-02-07 16:42:03 +0100

    Fixing typing and commenting

--- data/generator.py
+++ data/generator.py
@@ -1,3 +1,5 @@
+from typing import Generator, Any
+
 from PIL import Image
 
 from pathlib import Path
@@ -5,29 +7,33 @@ import os
 
 
 class ImageLoader:
-    def __init__(self, path: os.PathLike):
-        self.path: os.PathLike = path
+    """
+    Initialize a ImageLoader with the root path, the generator will find all images in all subdirectories of that path.
+    """
+
+    def __init__(self, path: os.PathLike[str]):
+        self.path: os.PathLike[str] = path
+
+    """
+    Convert a full path to a pillow Image
+    """
 
     @staticmethod
-    def load_image(image_path: Path) -> Image:
+    def load_image(image_path: Path) -> Image.Image | None:
         # Load an image using PIL
         try:
-            image: Image = Image.open(image_path)
+            image: Image.Image = Image.open(image_path)
             return image
         except Exception as e:
             print(f"Error loading image {image_path}: {e}")
             return None
 
-    def __iter__(self) -> Image:
-        # Generator that yields one image at a time
-        root: os.PathLike
-        dirs: list[os.PathLike]
-        files: list[os.PathLike]
+    def __iter__(self) -> Generator[Image.Image, Any, None]:
         for root, dirs, files in os.walk(self.path):
-            file: os.PathLike
+            file: str
             for file in files:
                 file_path: str = os.path.join(root, file)
-                image: Image = self.load_image(Path(file_path))
+                image: Image.Image | None = self.load_image(Path(file_path))
                 if image:
                     yield image
 
@@ -39,6 +45,6 @@ if __name__ == "__main__":
     image_loader = ImageLoader(Path(path_to_images))
 
     # Iterate through the images using the generator
-    img: Image
+    img: Image.Image
     for img in image_loader:
         img.show()  # Display the image (you can replace this with your own processing logic)
