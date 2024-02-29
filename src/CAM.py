7bb53ae2ddafd4b1ee9da0fb245ecdcd9f3a0c6e
OlofLindber9 <oloflind@student.chalmers.se>
2024-02-29 17:37:03 +0100

    created a ClassActivationMap

--- src/CAM.py
+++ src/CAM.py
@@ -1,13 +1,11 @@
-from torchvision import models
 import numpy as np
 import cv2
 import PIL.Image
-from pytorch_grad_cam import GradCAM,GradCAMPlusPlus
+from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image,preprocess_image
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from model import BinaryResNet50NotPreTrained
 import torch
-import os
 
 print("CAM process started")
 
