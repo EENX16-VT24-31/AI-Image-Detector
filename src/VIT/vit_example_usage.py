5fa4edacc6384f59f6b0dd1b55f72befeffdee37
maxsjodahl <max.sjodahl12@gmail.com>
2024-03-10 19:13:02 +0100

    removed generator.py

--- src/VIT/vit_example_usage.py
+++ src/VIT/vit_example_usage.py
@@ -72,10 +72,10 @@ def train_test(train_path: str,
 # Example usage of train_test
 if __name__ == "__main__":
 
-    train_path: str=r'C:\Users\maxsj\imagenet_ai_0419_vqdm\train'
-    val_path: str=r'C:\Users\maxsj\imagenet_ai_0419_vqdm\val'
-    val_ai_path: str= r'C:\Users\maxsj\imagenet_ai_0419_vqdm\val\ai\VQDM_1000_200_00_017_vqdm_00020.png'
-    val_nature_path: str= r'C:\Users\maxsj\imagenet_ai_0419_vqdm\val\nature\ILSVRC2012_val_00000744.JPEG'
+    train_path: str=r'C:\Users\maxsj\GenImage\vqdm\train'
+    val_path: str=r'C:\Users\maxsj\GenImage\vqdm\val'
+    val_ai_path: str= r'C:\Users\maxsj\GenImage\vqdm\val\ai\VQDM_1000_200_00_017_vqdm_00020.png'
+    val_nature_path: str= r'C:\Users\maxsj\GenImage\vqdm\val\nature\ILSVRC2012_val_00000744.JPEG'
 
     train_test(train_path=train_path,
                    val_path=val_path,
