efea37df48df160d81ff3ba5e46390a53e9665b3
erwinia <joel.lagg@gmail.com>
2024-03-21 23:41:38 +0100

    Joels PC config

--- src/VIT/config.py
+++ src/VIT/config.py
@@ -1,9 +1,9 @@
 from src.data import gen_image
 
 LEARNING_RATE: float=0.05
-EPOCHS: int=1
-IMAGE_COUNT: int=100
+EPOCHS: int=10
+IMAGE_COUNT: int | None = None
 WEIGHT_DECAY: float=0.03
 LOAD_PATH: str=r"C:\Users\maxsj\models\VIT_test_model.pth"
-BASE_PATH: str = r"C:\Users\maxsj\GenImage"
-GENERATORS: list[gen_image.Generator]=[gen_image.Generator.VQDM]
+BASE_PATH: str = r"F:\GenImage"
+GENERATORS: list[gen_image.Generator]=[gen_image.Generator.SD1_4]
